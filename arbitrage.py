906da050147873724b8458b09aa42cbdf191c478
Lloyd Hughes <hughes.lloyd@gmail.com>
2017-12-14 14:09:44 +0100

    Added logging transaction to a dictionary which I will later use to append to a pandas dataframe so I can save a log of the arbitrage over time

--- arbitrage.py
+++ arbitrage.py
@@ -8,6 +8,21 @@ from coinbase_ex import CoinbaseEx
 
 import argparse
 
+TRANSACTION_LOG_ITEM = {
+	"forex_type": "",
+	"local_type": "",
+	"token_type": "",
+	"initial": 0,
+	"forex": 0,
+	"forex_rate": 0,
+	"tokens": 0,
+	"token_buy_rate": 0,
+	"token_sell_rate": 0,
+	"local": 0,
+	"margin": 0,
+	"profit": 0,
+}
+
 def log(msg, verbose):
 	if verbose is True:
 		print(msg)
@@ -16,6 +31,13 @@ def log(msg, verbose):
 def arbitrage(amount, buy_ex, sell_ex, forex_ex=None, forex_fees=True, verbose=False, execute=False):
 	initial_amount = amount
 	forex_transaction = None
+
+	trans_store = TRANSACTION_LOG_ITEM.copy()
+
+	trans_store["forex_type"] = buy_ex.currency_from
+	trans_store["local_type"] = sell_ex.currency_from
+	trans_store["token_type"] = buy_ex.currency_to
+	trans_store["initial"] = amount
 
 	start_ex = buy_ex
 	if forex_ex:
@@ -28,14 +50,19 @@ def arbitrage(amount, buy_ex, sell_ex, forex_ex=None, forex_fees=True, verbose=F
 		log("Bought {:.2f} {} at a rate of {:.4f} {}".format(forex_transaction["bought"], forex_ex.currency_to,
 			forex_transaction["rate"],forex_ex.currency_from), verbose)
 		amount = forex_transaction["bought"]
+		trans_store["forex_rate"] = forex_transaction["rate"]
 
 	transaction = buy_ex.deposit(amount, include_fees=True, execute=execute)
 	log("Deposited {:.2f} {}".format(transaction["deposited"], buy_ex.currency_from), verbose)
+
+	trans_store["forex"] = transaction["deposited"]
 
 	transaction = buy_ex.buy(transaction["deposited"], include_fees=True, execute=execute)
 
 	log("Bought {:.6f} {} at a rate of {:.2f} {}".format(transaction["bought"], buy_ex.currency_to,
 		transaction["rate"],buy_ex.currency_from), verbose)
+
+	trans_store["token_buy_rate"] = transaction["rate"]
 
 	transaction = buy_ex.send(transaction["bought"], include_fees=True, execute=execute)
 	log("Sent {:.6f}{}".format(transaction["sent"], buy_ex.currency_to), verbose)
@@ -43,9 +70,13 @@ def arbitrage(amount, buy_ex, sell_ex, forex_ex=None, forex_fees=True, verbose=F
 	transaction = sell_ex.receive(transaction["sent"], include_fees=True, execute=execute)
 	log("Received {:.6f} {}".format(transaction["received"], sell_ex.currency_to), verbose)
 
+	trans_store["tokens"] = transaction["received"]
+
 	transaction = sell_ex.sell(transaction["received"], include_fees=True, execute=execute)
 	log("Sold {:.2f} {} at a rate of {:.2f} {}".format(transaction["sold"], sell_ex.currency_from,
 		transaction["rate"],sell_ex.currency_from), verbose)
+
+	trans_store["token_sell_rate"] = transaction["rate"]
 
 	transaction = sell_ex.withdrawl(transaction["sold"], include_fees=True, execute=execute)
 	log("Withdrew {:.2f} {}".format(transaction["withdrew"], sell_ex.currency_from), verbose)
@@ -57,9 +88,13 @@ def arbitrage(amount, buy_ex, sell_ex, forex_ex=None, forex_fees=True, verbose=F
 
 	margin = profit/initial_amount*100
 
+	trans_store["local"] = transaction["withdrew"]
+	trans_store["profit"] = profit
+	trans_store["margin"] = margin
+
 	log("Profit: {:.2f} {}\tMargin:{:.3f}".format(profit, sell_ex.currency_from, margin), verbose)
 
-	return(profit, margin)
+	return(profit, margin, trans_store)
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -69,7 +104,7 @@ def parse_args():
 	parser.add_argument("--token", default="BTC", choices=["BTC", "ETH"], help="Token to arbitrage")
 	parser.add_argument("--no_forex_buy", action="store_true", help="This will assume the arbitrage amount is already in Forex currency specified")
 	parser.add_argument("--forex_with_fees", action="store_true", help="Assumes we want to include the forex fees in the arbitrage amount")
-	parser.add_argument("--fmt", default="STDOUT", choices=["CSV", "STDOUT", "VERBOSE"], help="How to display the output")
+	parser.add_argument("--fmt", default="STD", choices=["CSV", "STD", "V"], help="How to display the output")
 
 	return(parser.parse_args())
 
@@ -88,7 +123,16 @@ def evaluate_arbitrage(args):
 	#buy_ex = CoinbaseEx(CB_KEY, CB_SECRET,"EUR","BTC")
 	sell_ex = Luno(LUNO_KEY,LUNO_SECRET, currency_from=args.local, currency_to=args.token)
 
-	profit, margin = arbitrage(args.amount, buy_ex, sell_ex, forex_ex=forex_ex, forex_fees=args.forex_with_fees, verbose=True, execute=False)
+	verbose = False
+	if args.fmt == "VERBOSE":
+		verbose = True
+
+	profit, margin, transaction = arbitrage(args.amount, buy_ex, sell_ex, forex_ex=forex_ex, forex_fees=args.forex_with_fees, verbose=verbose, execute=False)
+
+	if args.fmt == "STD":
+		print(transaction)
+	elif args.fmt == "CSV":
+
 
 if __name__ == "__main__":
 	args = parse_args()
